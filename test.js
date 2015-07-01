8e676106bea1327da0c4ac692825c35a25b48aa9
Erin <erindb@stanford.edu>
2015-07-01 14:43:18 -0700

    more tutorial

--- test.js
+++ test.js
@@ -23,45 +23,90 @@ var pdf = function(data, container, width, height) {
 	*/
 }
 
+function make_chart(selector, width, height, margin) {
+	var chart = d3.select(selector);
+	chart.attr("width", width + margin.left + margin.right)
+	chart.attr("height", height + margin.top + margin.bottom)
+	var chartElements = chart.append("g")
+	chartElements.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	return chart;
+}
+
+function make_bars(chart, data, x, y, width, height, margin) {
+	var bar = chart.selectAll(".bar");
+	var barUpdate = bar.data(data);
+
+	var barRect = barUpdate.enter().append("rect");
+	barRect.attr("class", "bar");
+	barRect.attr("x", function(d) { return x(d.name); });
+	barRect.attr("y", function(d) { return y(d.value); });
+	barRect.attr("height", function(d) { return height - y(d.value); });
+	barRect.attr("width", x.rangeBand());
+
+	return bar;
+}
+
+function make_y(height, data) {
+	var y = d3.scale.linear();
+	y.range([height, 0]);
+	y.domain([0, d3.max(data, function(d) { return d.value; })]);
+	return y;
+}
+
+function make_x(width, data) {
+	var x = d3.scale.ordinal();
+    x.domain(data.map(function(d) { return d.name; }));
+    x.rangeBands([0, width]);
+    return x;
+}
+function make_axis(scale, position) {
+	var axis = d3.svg.axis();
+    axis.scale(scale);
+    axis.orient(position);
+    return axis;
+}
+function draw_axes(chart, xAxis, yAxis, height) {
+	chart.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+	chart.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+		.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text("Frequency");
+}
+
 $(document).ready(function() {
 
 	// pdf(demo_pdf_data, "demo_pdf", 500, 300);
 
 	var data = [
-		{name: "Locke",    value:  4},
-		{name: "Reyes",    value:  8},
-		{name: "Ford",     value: 15},
-		{name: "Jarrah",   value: 16},
-		{name: "Shephard", value: 23},
-		{name: "Kwon",     value: 42}
+		{name: "A",    value:  4},
+		{name: "B",    value:  8},
+		{name: "C",    value: 15},
+		{name: "D",    value: 16},
+		{name: "E",    value: 23},
+		{name: "F",    value: 42}
 	];
 
-	var width = 420;
-	var barHeight = 20;
+	var container_width = 500;
+	var container_height = 300;
 
-	var x = d3.scale.linear();
-	x.domain([0, d3.max(data, function(d) {return d.value;})]);
-	x.range([0, width]);
+	var margin = {top: 20, right: 30, bottom: 30, left: 40};
+    var width = container_width - margin.left - margin.right;
+    var height = container_height - margin.top - margin.bottom;
 
-	var chart = d3.select(".chart");
-	chart.attr("width", width);
-	chart.attr("height", barHeight * data.length);
+    var x = make_x(width, data);
+	var y = make_y(height, data);
+	var chart = make_chart(".chart", width, height, margin);
+	var bars = make_bars(chart, data, x, y, width, height, margin);
 
-	var bar = chart.selectAll("g");
-	var barUpdate = bar.data(data);
-	var barEnter = barUpdate.enter().append("g")
-	barEnter.attr("transform", function(d, i) {
-		return "translate(0," + i * barHeight + ")";
-	});
-
-	var barRect = barEnter.append("rect");
-	barRect.attr("width", function(d) { return x(d.value); });
-	barRect.attr("height", barHeight - 1);
-
-	var barText = barEnter.append("text");
-	barText.attr("x", function(d) { return x(d.value) - 3; });
-	barText.attr("y", barHeight / 2);
-	barText.attr("dy", ".35em");
-	barText.text(function(d) { return d.value; });
-
+	var xAxis = make_axis(x, "bottom");
+	var yAxis = make_axis(y, "left");
+	draw_axes(chart, xAxis, yAxis, height);
 })
