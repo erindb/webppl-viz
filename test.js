74f7e4ff2c81225575f380ccff0008930a085c3b
Erin <erindb@stanford.edu>
2015-07-01 18:34:58 -0700

    density is probably wrong

--- test.js
+++ test.js
@@ -1,12 +1,23 @@
 var demo_pdf_data = [
-	{value: -100, score: -1.38629436112, category: "A"},
-	{value: -50,  score: -1.38629436112, category: "A"},
-	{value: -90,  score: -0.69314718056, category: "A"},
-	{value: -10,  score: -0.69314718056, category: "B"},
-	{value: -20,  score: -0.69314718056, category: "B"}
+	{value: .9999, probability: 0.25, category: "A"},
+	{value: -.50,  probability: 0.25, category: "A"},
+	{value: -.90,  probability: 0.5, category: "A"},
+	{value: -10,  probability: 0.5, category: "B"},
+	{value: -.20,  probability: 0.5, category: "B"}
 ]
 
-var pdf = function(data, container, width, height) {
+var make_chart = function(container_selector, margin, width, height) {
+	// initialize empty chart
+	var chart = d3.select(container_selector)
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		// for all the elements in the chart, move to allow desired margins
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	return chart;
+}
+
+var pdf = function(data, container_selector, container_width, container_height) {
 	/*
 	* draw histogram and/or density
 	* when domain is an interval in R
@@ -21,92 +32,150 @@ var pdf = function(data, container, width, height) {
 	* but there might be too many for that, in which case
 	* we should do a binned histogram.
 	*/
-}
 
-function make_chart(selector, width, height, margin) {
-	var chart = d3.select(selector);
-	chart.attr("width", width + margin.left + margin.right)
-	chart.attr("height", height + margin.top + margin.bottom)
-	var chartElements = chart.append("g")
-	chartElements.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-	return chart;
-}
+	var make_x_scale = function(chart, width, height, lowest, highest, position) {
+		var x = d3.scale.linear()
+			.range([0, width])
+			.domain([ lowest, highest ]);
+		var x_axis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+		var x_axis_drawn = chart.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(x_axis);
+		return x;
+	}
 
-function make_bars(chart, data, x, y, width, height, margin) {
-	var bar = chart.selectAll(".bar");
-	var barUpdate = bar.data(data);
+	var draw_histogram = function(chart, data, x, width, height) {
 
-	var barRect = barUpdate.enter().append("rect");
-	barRect.attr("class", "bar");
-	barRect.attr("x", function(d) { return x(d.name); });
-	barRect.attr("y", function(d) { return y(d.value); });
-	barRect.attr("height", function(d) { return height - y(d.value); });
-	barRect.attr("width", x.rangeBand());
+		var probabilities = data.map(function(x) {return x.probability;});
+		var highest_probability = d3.max(probabilities);
 
-	return bar;
-}
+		// histogram requires binned data
+		var n_bins = 20;
+		var bin_width = (highest - lowest)/n_bins;
+		var hist_data = [];
+		for (var i=0; i<n_bins; i++) {
+			var total_probability = data.reduce(function(prev,current) {
+				if (current.value >= i*bin_width+lowest &
+					(current.value < (i+1)*bin_width+lowest |
+						(current.value == highest & i == n_bins - 1))) {
+					return  +(current.probability) + prev;
+				}
+				return +prev;
+			}, 0);
+			if (total_probability > highest_probability) {
+				// one of the histogram probabilities might
+				// be the highest y value.
+				highest_probability = total_probability;
+			}
+			hist_data.push({
+				value: i*bin_width + lowest,
+				probability: total_probability
+			})
+		}
 
-function make_y(height, data) {
-	var y = d3.scale.linear();
-	y.range([height, 0]);
-	y.domain([0, d3.max(data, function(d) { return d.value; })]);
-	return y;
-}
+		var y = d3.scale.linear()
+			.range([height, 0]);
+		y.domain([0, highest_probability]);
 
-function make_x(width, data) {
-	var x = d3.scale.ordinal();
-    x.domain(data.map(function(d) { return d.name; }));
-    x.rangeBands([0, width]);
-    return x;
-}
-function make_axis(scale, position) {
-	var axis = d3.svg.axis();
-    axis.scale(scale);
-    axis.orient(position);
-    return axis;
-}
-function draw_axes(chart, xAxis, yAxis, height) {
-	chart.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
-	chart.append("g")
-		.attr("class", "y axis")
-		.call(yAxis)
-		.append("text")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", ".71em")
-			.style("text-anchor", "end")
-			.text("Frequency");
+		chart.selectAll(".bar")
+			.data(hist_data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x(d.value); })
+			.attr("y", function(d) { return y(d.probability); })
+			.attr("height", function(d) { return height - y(d.probability); })
+			.attr("width", width/n_bins);
+	}
+
+	var draw_density_plot = function(chart, height, data, x) {	
+		var densify_data = function(data) {
+			var values = data.map(function(x) {return x.value;});
+			var lowest = d3.min(values);
+			var highest = d3.max(values);
+
+			// do kernel density estimate
+			// highest_probability = ...
+			var density_data = [];
+
+			function epanechnikovKernel(u) {
+				return Math.abs(u) <= 1 ? .75 * (1 - u * u) : 0;
+			}
+
+			// get optimal bandwidth
+			// HT http://en.wikipedia.org/wiki/Kernel_density_estimation#Practical_estimation_of_the_bandwidth
+			// var mean = erp.expectation();
+			var n = data.length; // this is too low
+			var mean = data.reduce(function(acc, x) {
+				return acc + (x.probability * x.value);
+			}, 0);
+			var sd = Math.sqrt(data.reduce(function(acc, x) {
+				return acc + (x.probability * Math.pow(x.value - mean, 2));
+			}, 0));
+
+			var bandwidth = 1.06 * sd * Math.pow(n, -0.2);
+
+			var numBins = (highest - lowest) / bandwidth;
+
+			for (var i = 0; i <= numBins; i++) {
+				var x = lowest + bandwidth * i;
+				var kernel_sum = 0;
+				for (var j = 0; j < data.length; j++) {
+					var datum = data[j];
+					kernel_sum += epanechnikovKernel((x - datum.value) / bandwidth) * datum.probability;
+				}
+				density_data.push({
+					value: x,
+					probability: kernel_sum / (n * bandwidth)
+					});
+			}
+			return density_data;
+		}
+		var density_data = densify_data(data);
+		var y_density = d3.scale.linear()
+			.range([height, 0]);
+		y_density.domain([0, d3.max(density_data.map(function(x) {return x.probability;}))]);
+		var line = d3.svg.line()
+		    .x(function(d) { return x(d.value); })
+		    .y(function(d) { return y_density(d.probability); });
+		chart.append("path")
+	    	.datum(density_data)
+	    	.attr("class", "line")
+			.attr("d", line);
+	}
+
+	var draw_hist = true;
+	var draw_density = true;
+
+	// overall chart properties, for fitting plot within container
+	var margin = {top: 20, right: 30, bottom: 30, left: 40};
+	var width = container_width - margin.left - margin.right;
+	var height = container_height - margin.top - margin.bottom;
+
+	// initialize empty chart
+	var chart = make_chart(container_selector, margin, width, height);
+
+	// extract some useful properties of the data
+	var values = data.map(function(x) {return x.value;});
+	var lowest = d3.min(values);
+	var highest = d3.max(values);
+
+	// x-axis should be continuous, because actual data
+	// domain is continuous.
+	var x = make_x_scale(chart, width, height, lowest, highest);
+
+	// make histogram bars
+	if (draw_hist) {
+		draw_histogram(chart, data, x, width, height);
+	}
+	if (draw_density) {
+		draw_density_plot(chart, height, data, x)
+	}
+
 }
 
 $(document).ready(function() {
-
-	// pdf(demo_pdf_data, "demo_pdf", 500, 300);
-
-	var data = [
-		{name: "A",    value:  4},
-		{name: "B",    value:  8},
-		{name: "C",    value: 15},
-		{name: "D",    value: 16},
-		{name: "E",    value: 23},
-		{name: "F",    value: 42}
-	];
-
-	var container_width = 500;
-	var container_height = 300;
-
-	var margin = {top: 20, right: 30, bottom: 30, left: 40};
-    var width = container_width - margin.left - margin.right;
-    var height = container_height - margin.top - margin.bottom;
-
-    var x = make_x(width, data);
-	var y = make_y(height, data);
-	var chart = make_chart(".chart", width, height, margin);
-	var bars = make_bars(chart, data, x, y, width, height, margin);
-
-	var xAxis = make_axis(x, "bottom");
-	var yAxis = make_axis(y, "left");
-	draw_axes(chart, xAxis, yAxis, height);
+	pdf(demo_pdf_data, "#pdf_demo", 500, 300);
 })
