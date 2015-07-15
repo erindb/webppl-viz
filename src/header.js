41e5209a780a238e2b2787618e5e47f1ab643fd4
Erin <erindb@stanford.edu>
2015-07-15 12:50:31 -0700

    add heatmaps for discrete variables

--- src/header.js
+++ src/header.js
@@ -153,6 +153,79 @@ module.exports = function(env){
       .attr("width", width/n_bins);
   }
 
+  var heat_map = function(cat1_values, cat2_values, probabilities, container_selector, container_width, container_height, category1, category2) {
+    //size of circle is propto score
+    //opacity is fairly low
+
+    var data = make_2cat_data(cat1_values, cat2_values, probabilities, category1, category2);
+    // console.log(data);
+
+    // overall chart properties, for fitting plot within container
+    var width = container_width - margin.left - margin.right;
+    var height = container_height - margin.top - margin.bottom;
+
+    // initialize empty chart
+    var chart = make_chart(container_selector, margin, width, height);
+
+    // cat1 is on X axis
+    var cat1_scale = d3.scale.ordinal()
+        .domain(cat1_values)
+        .rangeBands([0, width], 0.1);
+    var cat1_Axis = d3.svg.axis()
+      .scale(cat1_scale)
+      .orient("bottom");
+    var x_axis_drawn = chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(cat1_Axis);
+    x_axis_drawn.append("text")
+      .attr("x", width/2)
+      .attr("dy", "3em")
+      .style("text-anchor", "end")
+      .text(category1);
+
+    // cat2 is on Y axis
+    var cat2_scale = d3.scale.ordinal()
+        .domain(cat2_values)
+        .rangeBands([height, 0], 0.1);
+    var cat2_Axis = d3.svg.axis()
+      .scale(cat2_scale)
+      .orient("left");
+    var y_axis_drawn = chart.append("g")
+      .attr("class", "y axis")
+      .call(cat2_Axis);
+    y_axis_drawn.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("x", -height/2)
+      .attr("dy", "-4em")
+      .style("text-anchor", "end")
+      .text(category2);
+
+    var minprob = d3.min(probabilities);
+    var maxprob = d3.max(probabilities);
+    // console.log(probabilities);
+    // console.log(_.map(data, function(datum) {return datum.probability;}));
+    // console.log(maxprob);
+    if (minprob == maxprob) {
+      var color_scale = function(prob) {return "steelblue"};
+    } else {
+      var color_scale = d3.scale.linear()
+        .domain([minprob, maxprob])
+        .range(["white", "steelblue"]);
+    }
+
+    chart.selectAll(".tile")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "tile")
+      .attr("x", function(d) { return cat1_scale(d[category1]); })
+      .attr("width", function(d) { return cat1_scale.rangeBand(); })
+      .attr("y", function(d) { return cat2_scale(d[category2]); })
+      .attr("height", function(d) { return cat2_scale.rangeBand(); })
+      .style("fill", function(d) { return color_scale(d.probability); });
+  }
+
   var scatter = function(cat1_values, cat2_values, probabilities, container_selector, container_width, container_height, category1, category2) {
     //size of circle is propto score
     //opacity is fairly low
@@ -288,10 +361,10 @@ module.exports = function(env){
 
   function plotSingleVariable(values, probabilities, container_selector, container_width, container_height, category) {
     if (isNumeric(values)) {
-      console.log("isNumeric");
+      // console.log("isNumeric");
       pdf(values, probabilities, container_selector, container_width, container_height, category);
     } else {
-      console.log("is not Numeric");
+      // console.log("is not Numeric");
       discrete_probability_distribution(values, probabilities, container_selector, container_width, container_height, category);
     }
   }
@@ -299,10 +372,10 @@ module.exports = function(env){
   function vizPrint(store, k, a, x){
     var resultDiv = $(activeCodeBox.parent().find(".resultDiv"));
     resultDiv.show();
-    console.log(x);
+    // console.log(x);
     // if we can plot things, plot things
     if (isErpWithSupport(x)){
-      console.log("isErpWithSupport");
+      // console.log("isErpWithSupport");
       var params = Array.prototype.slice.call(arguments, 2);
       var labels = x.support(params);
       var scores = _.map(labels, function(label){return x.score(params, label);});
@@ -315,7 +388,7 @@ module.exports = function(env){
 
       // // what kind of plot should I show?
       if (isNiceObject(labels)) {
-        console.log("isNiceObject");
+        // console.log("isNiceObject");
         var categories = Object.keys(labels[0]);
 
         var result_div = d3.select(resultDivSelector);
@@ -345,7 +418,6 @@ module.exports = function(env){
               var values1 = labels.map(function(x) {return x[category1];});
               var values2 = labels.map(function(x) {return x[category2];});
               var probabilities = counts;
-
               // if both numeric, plot scatterplots
               if (isNumeric(values1) & isNumeric(values2)) {
                 // first, make container
@@ -356,22 +428,51 @@ module.exports = function(env){
                 var plot_tag = "scatter_" + category1 + "_" + category2;
                 var plot_container = result_div.append("svg")
                   .attr("class", plot_tag);
-                var cat1_values = [];
-                var cat2_values = [];
                 scatter(values1, values2, probabilities, resultDivSelector + " ." + plot_tag, graph_width/2, graph_height/2, category1, category2);
+              } else if (!isNumeric(values1) & !isNumeric(values2)) {
+                // first, we marginalize to these 2 variables only
+                var category_data = make_2cat_data(values1, values2, counts, category1, category2);
+                var category1values = _.unique(values1);
+                var category2values = _.unique(values2);
+                var heatmap_probabilities = _.flatten(_.map(category1values, function(cat1val) {
+                  return _.map(category2values, function(cat2val) {
+                    var relevant_data = _.filter(category_data, function(datum) {
+                      return datum[category1] == cat1val & datum[category2] == cat2val;
+                    });
+                    return (_.map(relevant_data, function(datum) { return datum.probability; })).reduce(function(a, b) {
+                      return a + b;
+                    });
+                  });
+                }));
+                var category1values_aligned_with_probs = _.flatten(_.map(category1values, function(cat1val) {
+                  return _.map(category2values, function(cat2val) {
+                    return cat1val;
+                  })
+                }));
+                var category2values_aligned_with_probs = _.flatten(_.map(category1values, function(cat1val) {
+                  return _.map(category2values, function(cat2val) {
+                    return cat2val;
+                  })
+                }));
+                // console.log(heatmap_probabilities);
+                var plot_tag = "heat_map_" + category1 + "_" + category2;
+                var plot_container = result_div.append("svg")
+                  .attr("class", plot_tag);
+                heat_map(category1values_aligned_with_probs, category2values_aligned_with_probs, heatmap_probabilities,
+                  resultDivSelector + " ." + plot_tag, graph_width/2, graph_height/2, category1, category2);
               }
             }
           }
         }
       } else {
-        console.log("is not NiceObject");
+        // console.log("is not NiceObject");
         var result_div = d3.select(resultDivSelector);
         var plot_div = result_div.append("svg")
           .attr("class", "single_variable_plot");
         plotSingleVariable(labels, counts, resultDivSelector + " .single_variable_plot", graph_width, graph_height, "")
       }
     } else {
-      console.log("is not ErpWithSupport");
+      // console.log("is not ErpWithSupport");
       //otherwise, stringify and print
       resultDiv.append(
       document.createTextNode(
